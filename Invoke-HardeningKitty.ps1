fa80db0d25594f4cb0d8b869bc86722f3caa1505
Glenn Matthys <glenn.matthys@e-crane.com>
2021-01-21 11:30:42 +0100

    Add function Get-IniContent

--- Invoke-HardeningKitty.ps1
+++ Invoke-HardeningKitty.ps1
@@ -244,6 +244,41 @@
         }
     }
 
+    Function Get-IniContent ($filePath) {
+
+        <#
+        .SYNOPSIS
+
+            Read a .ini file into a tree of hashtables
+
+        .NOTES
+
+            Original source see https://devblogs.microsoft.com/scripting/use-powershell-to-work-with-any-ini-file/
+        #>
+
+        $ini = @{}
+        switch -regex -file $FilePath
+        {
+            “^\[(.+)\]” { # Section
+                $section = $matches[1]
+                $ini[$section] = @{}
+                $CommentCount = 0
+            }
+            “^(;.*)$” { # Comment
+                $value = $matches[1]
+                $CommentCount = $CommentCount + 1
+                $name = “Comment” + $CommentCount
+                $ini[$section][$name] = $value
+            }
+            “(.+?)\s*=(.*)” { # Key
+                $name,$value = $matches[1..2]
+                $ini[$section][$name] = $value
+            }
+        }
+
+        return $ini
+    }
+
     #
     # Start Main
     #
